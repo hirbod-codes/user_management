0e4f7f0c49572fb2730e16ba488e79ceaacaf0ad
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-06-03 13:46:48 +0000

    Add Login method to IUserRepository

--- Data/User/IUserRepository.cs
+++ Data/User/IUserRepository.cs
@@ -16,6 +16,7 @@ public interface IUserRepository
     public Task<User?> RetrieveUserByLoginCredentials(string? email, string? username);
     public Task<User?> RetrieveUserForPasswordChange(string email);
     public Task<User?> RetrieveByTokenValue(string value);
+    public Task<bool?> Login(User user);
     public Task<bool?> UpdateVerificationSecret(string VerificationSecret, string email);
     public Task<bool?> Verify(ObjectId id);
     public Task<bool?> ChangePassword(string email, string hashedPassword);
