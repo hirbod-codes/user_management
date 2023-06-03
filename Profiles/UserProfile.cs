40ed9d3f62f53b5616aed222507ca53796babc24
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-06-03 13:14:54 +0000

    Update UserProfile.cs

--- Profiles/UserProfile.cs
+++ Profiles/UserProfile.cs
@@ -9,6 +9,8 @@ class UserProfile : Profile
     public UserProfile()
     {
         MapUserCreateDto();
+        MapUserRetrieveDto();
+        MapUserPrivilegesRetrieveDto();
     }
 
     private void MapUserCreateDto()
@@ -16,4 +18,30 @@ class UserProfile : Profile
         CreateMap<UserCreateDto, User>();
     }
 
+    private void MapUserRetrieveDto()
+    {
+        CreateMap<UserClient, UserClientRetrieveDto>();
+    }
+
+    private void MapUserPrivilegesRetrieveDto()
+    {
+        CreateMap<UserPrivileges, UserPrivilegesRetrieveDto>().ConvertUsing<UserPrivilegesConverter>();
+
+        CreateMap<Reader, ReaderRetrieveDto>();
+        CreateMap<Updater, UpdaterRetrieveDto>();
+        CreateMap<Deleter, DeleterRetrieveDto>();
+    }
+}
+
+public class UserPrivilegesConverter : ITypeConverter<UserPrivileges, UserPrivilegesRetrieveDto>
+{
+    public UserPrivilegesRetrieveDto Convert(UserPrivileges s, UserPrivilegesRetrieveDto d, ResolutionContext context) => new UserPrivilegesRetrieveDto()
+    {
+        Privileges = s.Privileges,
+        Readers = s.Readers.ToList().ConvertAll<ReaderRetrieveDto>(o => new ReaderRetrieveDto() { Author = o.Author, AuthorId = o.AuthorId.ToString(), IsPermitted = o.IsPermitted, Fields = o.Fields }).ToArray(),
+        AllReaders = s.AllReaders,
+        Updaters = s.Updaters.ToList().ConvertAll<UpdaterRetrieveDto>(o => new UpdaterRetrieveDto() { Author = o.Author, AuthorId = o.AuthorId.ToString(), IsPermitted = o.IsPermitted, Fields = o.Fields }).ToArray(),
+        AllUpdaters = s.AllUpdaters,
+        Deleters = s.Deleters.ToList().ConvertAll<DeleterRetrieveDto>(o => new DeleterRetrieveDto() { Author = o.Author, AuthorId = o.AuthorId.ToString(), IsPermitted = o.IsPermitted }).ToArray(),
+    };
 }
