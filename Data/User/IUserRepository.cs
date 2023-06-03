e5773522d7e52476e56bd49c6d2c77d26c593fbb
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-06-03 09:21:26 +0000

    Add Verify method to IUserRepository & UserRepository

--- Data/User/IUserRepository.cs
+++ Data/User/IUserRepository.cs
@@ -11,4 +11,5 @@ public interface IUserRepository
     public Task<User?> RetrieveUserByLoginCredentials(string? email, string? username);
     public Task<User?> RetrieveByTokenValue(string value);
     public Task<bool?> UpdateVerificationSecret(string VerificationSecret, string email);
+    public Task<bool?> Verify(ObjectId id);
 }
