6da97b41807b76f7e814b16c02196c6d0970293c
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-06-03 14:03:22 +0000

    Add a call for seeding database in Program.cs

--- Program.cs
+++ Program.cs
@@ -77,6 +77,7 @@ if (app.Environment.IsDevelopment())
     if (jsonObj["MongoDB"]["IsSeeded"] == false)
     {
         await MongoContext.Initialize(app.Services.GetService<IOptions<MongoContext>>()!);
+        await (new Seeder(app.Services.GetService<IOptions<MongoContext>>()!, app.Services.GetService<IUserRepository>()!, env.ContentRootPath)).Seed();
     }
     else
         System.Console.WriteLine("The database is already seeded.");
