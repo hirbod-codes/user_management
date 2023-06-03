461b7b9d5b703f9dc8d7314fbc8a866452dce811
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-06-03 14:02:19 +0000

    Forgot to commit added utilities and AutoMapper services

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using user_management.Authorization;
 using user_management.Authorization.Permissions;
 using user_management.Authorization.Roles;
 using user_management.Authorization.Scopes;
+using user_management.Utilities;
 using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,9 +19,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<IStringHelper, StringHelper>();
+builder.Services.AddSingleton<IAuthHelper, AuthHelper>();
+builder.Services.AddSingleton<INotificationHelper, NotificationHelper>();
+builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
 builder.Services.Configure<MongoContext>(builder.Configuration.GetSection("MongoDB"));
 builder.Services.AddSingleton<IMongoClient>(new MongoClient(builder.Configuration.GetSection("MongoDB").GetValue<string>("ConnectionString")));
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.Configure<JWTAuthenticationOptions>(builder.Configuration.GetSection("JWT"));
 builder.Services.AddSingleton<JWTAuthenticationOptions>();
 builder.Services.AddSingleton<IJWTAuthenticationHandler, JWTAuthenticationHandler>();
