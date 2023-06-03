ab6f68c7ce4e424200c362c82e2f132802e09df4
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-06-03 14:22:08 +0000

    Add Base64Encode & Base64Decode to IStringHelper

--- Utilities/IStringHelper.cs
+++ Utilities/IStringHelper.cs
@@ -2,6 +2,8 @@ namespace user_management.Utilities;
 
 public interface IStringHelper
 {
+    public string Base64Encode(string plainText);
+    public string Base64Decode(string base64EncodedData);
     public string? HashWithoutSalt(string input, string method = "SHA512");
     public string Hash(string str);
     public bool DoesHashMatch(string hashedStr, string rawStr);
--- Utilities/StringHelper.cs
+++ Utilities/StringHelper.cs
@@ -7,6 +7,18 @@ public class StringHelper : IStringHelper
 {
     public readonly char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
 
+    public string Base64Encode(string plainText)
+    {
+        byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+        return Convert.ToBase64String(plainTextBytes);
+    }
+
+    public string Base64Decode(string base64EncodedData)
+    {
+        byte[] base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+        return Encoding.UTF8.GetString(base64EncodedBytes);
+    }
+
     public string? HashWithoutSalt(string input, string method = "SHA512")
     {
         StringBuilder sb = new StringBuilder();
