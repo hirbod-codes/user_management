04632c6c42400624b99365ddb53641cc214f8074
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-06-04 15:38:47 +0000

    Update TokenController.cs

--- Controllers/TokenController.cs
+++ Controllers/TokenController.cs
@@ -45,16 +45,19 @@ public class TokenController : ControllerBase
         if (_authHelper.GetAuthenticationType(User) != "JWT" || tokenAuthDto.State.Length < 40 || tokenAuthDto.ResponseType != "code" || (new List<string>() { "SHA256", "SHA512" }).FirstOrDefault<string?>(s => s != null && s == tokenAuthDto.CodeChallengeMethod) == null) return BadRequest();
 
         string? id = await _authHelper.GetIdentifier(User, _userRepository);
-        if (id == null) return NotFound();
-        if (!ObjectId.TryParse(id, out ObjectId userId)) return BadRequest();
+        if (id == null) return Unauthorized();
+        if (!ObjectId.TryParse(id, out ObjectId userId)) return Unauthorized();
 
-        if (!ObjectId.TryParse(tokenAuthDto.ClientId, out ObjectId clientObjectId)) return BadRequest();
+        if (!ObjectId.TryParse(tokenAuthDto.ClientId, out ObjectId clientObjectId))
+            return NotFound("We don't have a client with this client id: " + tokenAuthDto.ClientId);
 
-        User? user = await _userRepository.RetrieveById(userId, userId); if (user == null) return NotFound();
+        User? user = await _userRepository.RetrieveById(userId, userId);
+        if (user == null) return Unauthorized();
 
         TokenPrivileges scope = _mapper.Map<TokenPrivileges>(tokenAuthDto.Scope);
 
-        if ((await _clientRepository.RetrieveByIdAndRedirectUrl(clientObjectId, tokenAuthDto.RedirectUrl)) == null) return NotFound();
+        if ((await _clientRepository.RetrieveByIdAndRedirectUrl(clientObjectId, tokenAuthDto.RedirectUrl)) == null)
+            return NotFound("We don't have a client with this client id: " + tokenAuthDto.ClientId + " and this redirect url: " + tokenAuthDto.RedirectUrl);
 
         string code = null!;
         bool again = false;
@@ -91,19 +94,28 @@ public class TokenController : ControllerBase
     {
         if (tokenCreateDto.GrantType != "authorization_code") return BadRequest("only 'authorization_code' grant type is supported");
 
-        Client? client = await _clientRepository.RetrieveByIdAndRedirectUrl(ObjectId.Parse(tokenCreateDto.ClientId), tokenCreateDto.RedirectUrl);
-        if (client == null) return NotFound();
+        if (!ObjectId.TryParse(tokenCreateDto.ClientId, out ObjectId clientObjectId))
+            return NotFound("We don't have a client with this client id: " + tokenCreateDto.ClientId);
+
+        Client? client = await _clientRepository.RetrieveByIdAndRedirectUrl(clientObjectId, tokenCreateDto.RedirectUrl);
+        if (client == null)
+            return NotFound("We don't have a client with this client id: " + tokenCreateDto.ClientId + " and this redirect url: " + tokenCreateDto.RedirectUrl);
 
         User? user = await _userRepository.RetrieveByClientIdAndCode((ObjectId)client.Id!, tokenCreateDto.Code);
-        if (user == null) return NotFound();
+        if (user == null) return NotFound("We couldn't find your account.");
 
         UserClient? userClient = user.Clients.ToList().FirstOrDefault<UserClient?>(uc => uc != null && uc.ClientId == client.Id, null);
-        if (userClient == null) return NotFound();
+        if (userClient == null) return NotFound("You haven't authorized a client with the provided client id.");
 
         RefreshToken? refreshToken = userClient.RefreshToken;
-        if (refreshToken == null) return NotFound();
-        if (refreshToken.ExpirationDate! < _dateTimeProvider.ProvideUtcNow() || (refreshToken.CodeExpiresAt != null && refreshToken.CodeExpiresAt < _dateTimeProvider.ProvideUtcNow())) return BadRequest();
-        if (_stringHelper.HashWithoutSalt(tokenCreateDto.CodeVerifier, refreshToken.CodeChallengeMethod!) != _stringHelper.Base64Decode(refreshToken.CodeChallenge!)) return BadRequest();
+        if (refreshToken == null)
+            return NotFound("We couldn't find your refresh token.");
+        if (refreshToken.ExpirationDate! < _dateTimeProvider.ProvideUtcNow())
+            return BadRequest("Your refresh token has been expired.");
+        if (refreshToken.CodeExpiresAt != null && refreshToken.CodeExpiresAt < _dateTimeProvider.ProvideUtcNow())
+            return BadRequest("Your code has been expired.");
+        if (_stringHelper.HashWithoutSalt(tokenCreateDto.CodeVerifier, refreshToken.CodeChallengeMethod!) != _stringHelper.Base64Decode(refreshToken.CodeChallenge!))
+            return BadRequest("Your code verifier is invalid.");
 
         string tokenValue = null!;
         using (IClientSessionHandle session = await _mongoClient.StartSessionAsync())
@@ -113,8 +125,8 @@ public class TokenController : ControllerBase
             session.StartTransaction(transactionOptions);
 
             bool? userResult = await _userRepository.AddTokenPrivileges(user, (ObjectId)client.Id, refreshToken.TokenPrivileges!, session);
-            if (userResult == null) return NotFound();
-            if (userResult == false) return Problem();
+            if (userResult == null) return NotFound("We couldn't find your account.");
+            if (userResult == false) return Problem("We couldn't add your permissions to the requested client.");
 
             bool again = false;
             int safety = 0;
@@ -127,11 +139,13 @@ public class TokenController : ControllerBase
                     bool? addTokenResult = await _userRepository.AddToken(user, (ObjectId)client.Id!, _stringHelper.HashWithoutSalt(tokenValue)!, _dateTimeProvider.ProvideUtcNow().AddHours(TOKEN_EXPIRATION), session);
                     if (addTokenResult == null)
                     {
-                        await session.AbortTransactionAsync(); return NotFound();
+                        await session.AbortTransactionAsync();
+                        return NotFound("We couldn't find your account's information.");
                     }
                     if (addTokenResult == false)
                     {
-                        await session.AbortTransactionAsync(); return Problem();
+                        await session.AbortTransactionAsync();
+                        return Problem("We couldn't generate a token for you.");
                     }
                     again = false;
                 }
@@ -142,7 +156,7 @@ public class TokenController : ControllerBase
             if (safety >= 200)
             {
                 await session.AbortTransactionAsync();
-                return Problem();
+                return Problem("We couldn't generate a token for you.");
             }
 
             await session.CommitTransactionAsync();
