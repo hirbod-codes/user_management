95f61c2b7501dbafd840e93c8fc1b78428013505
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-07-18 10:29:52 +0100

    Update UserCreateDto.cs

--- Dtos/User/UserCreateDto.cs
+++ Dtos/User/UserCreateDto.cs
@@ -9,7 +9,6 @@ public class UserCreateDto
     public string? LastName { get; set; }
     [EmailAddress]
     public string Email { get; set; } = null!;
-    [Phone]
     public string? PhoneNumber { get; set; }
     public string Username { get; set; } = null!;
     public string Password { get; set; } = null!;
