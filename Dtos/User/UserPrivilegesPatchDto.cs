167f1299bbbc91826eef37e02240e822872f4c09
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-08-04 16:50:38 +0100

    Update UserPrivilegesPatchDto.cs

--- Dtos/User/UserPrivilegesPatchDto.cs
+++ Dtos/User/UserPrivilegesPatchDto.cs
@@ -5,8 +5,8 @@ using user_management.Models;
 public class UserPrivilegesPatchDto
 {
     public ReaderPatchDto[]? Readers { get; set; } = null;
-    public AllReaders? AllReaders { get; set; }
+    public AllReaders? AllReaders { get; set; } = null;
     public UpdaterPatchDto[]? Updaters { get; set; } = null;
-    public AllUpdaters? AllUpdaters { get; set; }
+    public AllUpdaters? AllUpdaters { get; set; } = null;
     public DeleterPatchDto[]? Deleters { get; set; } = null;
 }
