3b66280adf50071b8e3306d2c429cbd03db286f3
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-08-18 22:01:20 +0100

    Update UserControllerService gRPC service

--- GrpcServices/UserControllerService.cs
+++ GrpcServices/UserControllerService.cs
@@ -2,12 +2,17 @@ namespace user_management.GrpcServices;
 
 using AutoMapper;
 using Grpc.Core;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+using user_management.Models;
+using user_management.Services;
 using user_management.Services.Data.User;
 using user_management.Utilities;
 
 public class UserControllerService : UserController.UserControllerBase
 {
     private const int EXPIRATION_MINUTES = 6;
+    private readonly UserManagement _userManagement;
     private readonly IMapper _mapper;
     private readonly IUserRepository _userRepository;
     private readonly IAuthHelper _authHelper;
@@ -15,8 +20,9 @@ public class UserControllerService : UserController.UserControllerBase
     private readonly INotificationHelper _notificationHelper;
     private readonly IDateTimeProvider _dateTimeProvider;
 
-    public UserControllerService(IUserRepository userRepository, IMapper mapper, IStringHelper stringHelper, INotificationHelper notificationHelper, IAuthHelper authHelper, IDateTimeProvider dateTimeProvider)
+    public UserControllerService(IUserRepository userRepository, IMapper mapper, IStringHelper stringHelper, INotificationHelper notificationHelper, IAuthHelper authHelper, IDateTimeProvider dateTimeProvider, UserManagement userManagement)
     {
+        _userManagement = userManagement;
         _mapper = mapper;
         _userRepository = userRepository;
         _authHelper = authHelper;
@@ -24,8 +30,20 @@ public class UserControllerService : UserController.UserControllerBase
         _stringHelper = stringHelper;
         _dateTimeProvider = dateTimeProvider;
     }
-    public override Task<GetResponse> Get(GetRequest request, ServerCallContext context)
+    public override async Task<GetResponse> Get(GetRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new GetResponse() { User = new Google.Protobuf.WellKnownTypes.Any() { TypeUrl = "#", Value = Google.Protobuf.ByteString.CopyFrom(Convert.FromBase64String((new StringHelper()).Base64Encode("content"))) } });
+        User user = await _userManagement.RetrieveById(request.AuthorId, request.Id, request.IsClient);
+
+        if (!ObjectId.TryParse(request.AuthorId, out ObjectId authorObjectId)) throw new ArgumentException();
+
+        object? content = user.GetReadable(authorObjectId, _mapper, request.IsClient);
+        return new GetResponse()
+        {
+            User = new Google.Protobuf.WellKnownTypes.Any()
+            {
+                TypeUrl = "#",
+                Value = Google.Protobuf.ByteString.CopyFrom(Convert.FromBase64String((new StringHelper()).Base64Encode(JsonConvert.SerializeObject(content))))
+            }
+        };
     }
 }
