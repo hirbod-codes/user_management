3e16180890d90730af8a16a8334f2d75e6b0e4b0
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-08-23 15:01:23 +0100

    Update IUserRepository.cs

--- Services/Data/User/IUserRepository.cs
+++ Services/Data/User/IUserRepository.cs
@@ -7,13 +7,13 @@ using user_management.Models;
 public interface IUserRepository
 {
     public Task<User?> Create(User user);
-    public Task<User?> RetrieveById(ObjectId id);
-    public Task<User?> RetrieveById(ObjectId actorId, ObjectId id, bool forClients = false);
     public Task<User?> RetrieveByFullNameForExistenceCheck(string firstName, string middleName, string lastName);
     public Task<User?> RetrieveByUsernameForExistenceCheck(string username);
     public Task<User?> RetrieveByEmailForExistenceCheck(string email);
     public Task<User?> RetrieveByPhoneNumberForExistenceCheck(string phoneNumber);
-    public Task<List<User>> Retrieve(ObjectId actorId, string logicsString, int limit, int iteration, string? sortBy, bool ascending = true, bool forClients = false);
+    public Task<User?> RetrieveById(ObjectId id);
+    public Task<PartialUser?> RetrieveById(ObjectId actorId, ObjectId id, bool forClients = false);
+    public Task<List<PartialUser>> Retrieve(ObjectId actorId, string logicsString, int limit, int iteration, string? sortBy, bool ascending = true, bool forClients = false);
     public Task<User?> RetrieveByIdForAuthentication(ObjectId userId);
     public Task<User?> RetrieveByIdForAuthorization(ObjectId id);
     public Task<User?> RetrieveUserByLoginCredentials(string? email, string? username);
@@ -24,7 +24,7 @@ public interface IUserRepository
     public Task<User?> RetrieveByClientIdAndCode(ObjectId clientId, string code);
     public Task<User?> RetrieveByRefreshTokenValue(string value);
     public Task<User?> RetrieveByTokenValue(string value);
-    public Task<bool?> Login(User user);
+    public Task<bool?> Login(ObjectId userId);
     public Task<bool?> UpdateVerificationSecret(string VerificationSecret, string email);
     public Task<bool?> UpdateVerificationSecretForActivation(string VerificationSecret, string email);
     public Task<bool?> UpdateVerificationSecretForPasswordChange(string VerificationSecret, string email);
@@ -34,12 +34,12 @@ public interface IUserRepository
     public Task<bool?> ChangePhoneNumber(string email, string phoneNumber);
     public Task<bool?> ChangeEmail(string email, string newEmail);
     public Task<bool?> Logout(ObjectId id);
-    public Task<bool?> RemoveClient(User user, ObjectId clientId, ObjectId authorId, bool isClient);
-    public Task<bool?> RemoveAllClients(User user, ObjectId authorId, bool isClient);
-    public Task<bool?> AddToken(User user, ObjectId clientId, string tokenValue, DateTime expirationDate, IClientSessionHandle? session = null);
-    public Task<bool?> AddTokenPrivileges(User user, ObjectId clientId, TokenPrivileges tokenPrivileges, IClientSessionHandle? session = null);
-    public Task<bool?> AddClientById(User user, ObjectId clientId, ObjectId actorId, bool forClients, TokenPrivileges tokenPrivileges, DateTime refreshTokenExpiration, string refreshTokenValue, DateTime codeExpiresAt, string code, string codeChallenge, string codeChallengeMethod);
-    public Task<bool?> UpdateUserPrivileges(User author);
+    public Task<bool?> RemoveClient(ObjectId userId, ObjectId clientId, ObjectId authorId, bool isClient);
+    public Task<bool?> RemoveAllClients(ObjectId userId, ObjectId authorId, bool isClient);
+    public Task<bool?> AddToken(ObjectId userId, ObjectId authorId, ObjectId clientId, string tokenValue, DateTime expirationDate, IClientSessionHandle? session = null);
+    public Task<bool?> AddTokenPrivilegesToUser(ObjectId userId, ObjectId authorId, ObjectId clientId, TokenPrivileges tokenPrivileges, IClientSessionHandle? session = null);
+    public Task<bool?> AddClientById(ObjectId userId, ObjectId clientId, ObjectId actorId, TokenPrivileges tokenPrivileges, DateTime refreshTokenExpiration, string refreshTokenValue, DateTime codeExpiresAt, string code, string codeChallenge, string codeChallengeMethod);
+    public Task<bool?> UpdateUserPrivileges(ObjectId authorId, ObjectId userId, UserPrivileges userPrivileges);
     public Task<bool?> Update(ObjectId actorId, string filtersString, string updatesString, bool forClients = false);
     public Task<bool?> Delete(ObjectId actorId, ObjectId id, bool forClients = false);
 }
