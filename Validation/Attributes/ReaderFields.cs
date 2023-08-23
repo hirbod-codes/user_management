4444134c6dbee33380b5f857b357a187325e8f71
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-08-23 15:05:47 +0100

    Update ReaderFields.cs

--- Validation/Attributes/ReaderFields.cs
+++ Validation/Attributes/ReaderFields.cs
@@ -5,5 +5,5 @@ namespace user_management.Validation.Attributes;
 
 public class ReaderFields : ValidationAttribute
 {
-    public override bool IsValid(object? value) => ((Field[])value!).Where(f => User.GetDefaultReadableFields().FirstOrDefault<Field?>(ff => ff != null && ff.Name == f.Name) == null).Count() == 0;
+    public override bool IsValid(object? value) => ((Field[])value!).Where(f => User.GetReadableFields().FirstOrDefault<Field?>(ff => ff != null && ff.Name == f.Name) == null).Count() == 0;
 }
