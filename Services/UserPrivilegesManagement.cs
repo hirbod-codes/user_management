9bf161d8b47be25626d9a130625504d2256ed33c
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-08-23 15:02:19 +0100

    Update UserPrivilegesManagement.cs

--- Services/UserPrivilegesManagement.cs
+++ Services/UserPrivilegesManagement.cs
@@ -19,87 +19,92 @@ public class UserPrivilegesManagement : IUserPrivilegesManagement
         _mapper = mapper;
     }
 
-    public async Task UpdateReaders(string authorId, UserPrivilegesPatchDto dto)
+    public async Task UpdateReaders(string authorId, string userId, UserPrivilegesPatchDto dto)
     {
+        if (!ObjectId.TryParse(userId, out ObjectId userObjectId)) throw new ArgumentException("userId");
         if (!ObjectId.TryParse(authorId, out ObjectId authorObjectId)) throw new ArgumentException("authorId");
 
         if (dto.Readers == null) throw new ArgumentException("dto");
 
-        User? author = await _userRepository.RetrieveById(authorObjectId);
-        if (author == null) throw new DataNotFoundException();
-        if (author.UserPrivileges == null) throw new OperationException();
+        User? user = await _userRepository.RetrieveById(userObjectId);
+        if (user == null) throw new DataNotFoundException();
+        if (user.UserPrivileges == null) throw new OperationException();
 
-        author.UserPrivileges.Readers = dto.Readers.ToList().ConvertAll<Reader>(rpd => _mapper.Map<Reader>(rpd)).ToArray();
+        user.UserPrivileges.Readers = dto.Readers.ToList().ConvertAll<Reader>(rpd => _mapper.Map<Reader>(rpd)).ToArray();
 
-        bool? r = await _userRepository.UpdateUserPrivileges(author);
+        bool? r = await _userRepository.UpdateUserPrivileges(authorObjectId, userObjectId, user.UserPrivileges);
         if (r == null) throw new DataNotFoundException();
         if (r == false) throw new OperationException();
     }
 
-    public async Task UpdateAllReaders(string authorId, UserPrivilegesPatchDto dto)
+    public async Task UpdateAllReaders(string authorId, string userId, UserPrivilegesPatchDto dto)
     {
-        if (!ObjectId.TryParse(authorId, out ObjectId authorObjectId))  throw new ArgumentException("authorId");
+        if (!ObjectId.TryParse(userId, out ObjectId userObjectId)) throw new ArgumentException("userId");
+        if (!ObjectId.TryParse(authorId, out ObjectId authorObjectId)) throw new ArgumentException("authorId");
 
-        if (dto.AllReaders == null)  throw new ArgumentException("dto");
+        if (dto.AllReaders == null) throw new ArgumentException("dto");
 
-        User? author = await _userRepository.RetrieveById(authorObjectId);
-        if (author == null) throw new DataNotFoundException();
-        if (author.UserPrivileges == null) throw new OperationException();
+        User? user = await _userRepository.RetrieveById(userObjectId);
+        if (user == null) throw new DataNotFoundException();
+        if (user.UserPrivileges == null) throw new OperationException();
 
-        author.UserPrivileges.AllReaders = _mapper.Map<AllReaders>(dto.AllReaders);
+        user.UserPrivileges.AllReaders = _mapper.Map<AllReaders>(dto.AllReaders);
 
-        bool? r = await _userRepository.UpdateUserPrivileges(author);
+        bool? r = await _userRepository.UpdateUserPrivileges(authorObjectId, userObjectId, user.UserPrivileges);
         if (r == null) throw new DataNotFoundException();
         if (r == false) throw new OperationException();
     }
 
-    public async Task UpdateUpdaters(string authorId, UserPrivilegesPatchDto dto)
+    public async Task UpdateUpdaters(string authorId, string userId, UserPrivilegesPatchDto dto)
     {
-        if (!ObjectId.TryParse(authorId, out ObjectId authorObjectId))  throw new ArgumentException("authorId");
+        if (!ObjectId.TryParse(userId, out ObjectId userObjectId)) throw new ArgumentException("userId");
+        if (!ObjectId.TryParse(authorId, out ObjectId authorObjectId)) throw new ArgumentException("authorId");
 
-        if (dto.Updaters == null)  throw new ArgumentException("dto");
+        if (dto.Updaters == null) throw new ArgumentException("dto");
 
-        User? author = await _userRepository.RetrieveById(authorObjectId);
-        if (author == null) throw new DataNotFoundException();
-        if (author.UserPrivileges == null) throw new OperationException();
+        User? user = await _userRepository.RetrieveById(userObjectId);
+        if (user == null) throw new DataNotFoundException();
+        if (user.UserPrivileges == null) throw new OperationException();
 
-        author.UserPrivileges.Updaters = dto.Updaters.ToList().ConvertAll<Updater>(rpd => _mapper.Map<Updater>(rpd)).ToArray();
+        user.UserPrivileges.Updaters = dto.Updaters.ToList().ConvertAll<Updater>(rpd => _mapper.Map<Updater>(rpd)).ToArray();
 
-        bool? r = await _userRepository.UpdateUserPrivileges(author);
+        bool? r = await _userRepository.UpdateUserPrivileges(authorObjectId, userObjectId, user.UserPrivileges);
         if (r == null) throw new DataNotFoundException();
         if (r == false) throw new OperationException();
     }
 
-    public async Task UpdateAllUpdaters(string authorId, UserPrivilegesPatchDto dto)
+    public async Task UpdateAllUpdaters(string authorId, string userId, UserPrivilegesPatchDto dto)
     {
-        if (!ObjectId.TryParse(authorId, out ObjectId authorObjectId))  throw new ArgumentException("authorId");
+        if (!ObjectId.TryParse(userId, out ObjectId userObjectId)) throw new ArgumentException("userId");
+        if (!ObjectId.TryParse(authorId, out ObjectId authorObjectId)) throw new ArgumentException("authorId");
 
-        if (dto.AllUpdaters == null)  throw new ArgumentException("dto");
+        if (dto.AllUpdaters == null) throw new ArgumentException("dto");
 
-        User? author = await _userRepository.RetrieveById(authorObjectId);
-        if (author == null) throw new DataNotFoundException();
-        if (author.UserPrivileges == null) throw new OperationException();
+        User? user = await _userRepository.RetrieveById(userObjectId);
+        if (user == null) throw new DataNotFoundException();
+        if (user.UserPrivileges == null) throw new OperationException();
 
-        author.UserPrivileges.AllUpdaters = _mapper.Map<AllUpdaters>(dto.AllUpdaters);
+        user.UserPrivileges.AllUpdaters = _mapper.Map<AllUpdaters>(dto.AllUpdaters);
 
-        bool? r = await _userRepository.UpdateUserPrivileges(author);
+        bool? r = await _userRepository.UpdateUserPrivileges(authorObjectId, userObjectId, user.UserPrivileges);
         if (r == null) throw new DataNotFoundException();
         if (r == false) throw new OperationException();
     }
 
-    public async Task UpdateDeleters(string authorId, UserPrivilegesPatchDto dto)
+    public async Task UpdateDeleters(string authorId, string userId, UserPrivilegesPatchDto dto)
     {
-        if (!ObjectId.TryParse(authorId, out ObjectId authorObjectId))  throw new ArgumentException("authorId");
+        if (!ObjectId.TryParse(userId, out ObjectId userObjectId)) throw new ArgumentException("userId");
+        if (!ObjectId.TryParse(authorId, out ObjectId authorObjectId)) throw new ArgumentException("authorId");
 
-        if (dto.Deleters == null)  throw new ArgumentException("dto");
+        if (dto.Deleters == null) throw new ArgumentException("dto");
 
-        User? author = await _userRepository.RetrieveById(authorObjectId);
-        if (author == null) throw new DataNotFoundException();
-        if (author.UserPrivileges == null) throw new OperationException();
+        User? user = await _userRepository.RetrieveById(userObjectId);
+        if (user == null) throw new DataNotFoundException();
+        if (user.UserPrivileges == null) throw new OperationException();
 
-        author.UserPrivileges.Deleters = dto.Deleters.ToList().ConvertAll<Deleter>(rpd => _mapper.Map<Deleter>(rpd)).ToArray();
+        user.UserPrivileges.Deleters = dto.Deleters.ToList().ConvertAll<Deleter>(rpd => _mapper.Map<Deleter>(rpd)).ToArray();
 
-        bool? r = await _userRepository.UpdateUserPrivileges(author);
+        bool? r = await _userRepository.UpdateUserPrivileges(authorObjectId, userObjectId, user.UserPrivileges);
         if (r == null) throw new DataNotFoundException();
         if (r == false) throw new OperationException();
     }
