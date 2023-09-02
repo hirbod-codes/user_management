0a0c8ddd4aa0c277ea87d063e74acc9bf23f553a
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-09-02 16:57:04 +0100

    Update MongoContext.cs

--- Data/MongoContext.cs
+++ Data/MongoContext.cs
@@ -1,6 +1,6 @@
-using System;
 namespace user_management.Data;
 
+using System;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Options;
@@ -30,17 +30,35 @@ public class MongoContext
 
         await ClearDatabaseAsync(database);
 
-        FilterDefinitionBuilder<Models.User> fb = Builders<Models.User>.Filter;
+        await CreateClientsCollectionIndexes(clientCollection);
+        await CreateUsersCollectionIndexes(userCollection);
+    }
 
+    private static async Task CreateClientsCollectionIndexes(IMongoCollection<Models.Client> clientCollection)
+    {
+        // Secret
         IndexKeysDefinition<Models.Client> clientSecretIndex = Builders<Models.Client>.IndexKeys.Ascending(Models.Client.SECRET);
         await clientCollection.Indexes.CreateOneAsync(new CreateIndexModel<Models.Client>(clientSecretIndex, new CreateIndexOptions() { Unique = true }));
 
+        // RedirectUrl
+        IndexKeysDefinition<Models.Client> clientRedirectUrlIndex = Builders<Models.Client>.IndexKeys.Ascending(Models.Client.REDIRECT_URL);
+        await clientCollection.Indexes.CreateOneAsync(new CreateIndexModel<Models.Client>(clientRedirectUrlIndex, new CreateIndexOptions() { Unique = true }));
+    }
+
+    private static async Task CreateUsersCollectionIndexes(IMongoCollection<Models.User> userCollection)
+    {
+
+        FilterDefinitionBuilder<Models.User> fb = Builders<Models.User>.Filter;
+
+        // Email
         IndexKeysDefinition<Models.User> userEmailIndex = Builders<Models.User>.IndexKeys.Ascending(Models.User.EMAIL);
         await userCollection.Indexes.CreateOneAsync(new CreateIndexModel<Models.User>(userEmailIndex, new CreateIndexOptions<Models.User>() { Unique = true }));
 
+        // Username
         IndexKeysDefinition<Models.User> userUsernameIndex = Builders<Models.User>.IndexKeys.Ascending(Models.User.USERNAME);
         await userCollection.Indexes.CreateOneAsync(new CreateIndexModel<Models.User>(userUsernameIndex, new CreateIndexOptions<Models.User>() { Unique = true, }));
 
+        // PhoneNumber
         string userPhoneNumberIndexField = Models.User.PHONE_NUMBER;
         IndexKeysDefinition<Models.User> userPhoneNumberIndex = Builders<Models.User>.IndexKeys.Ascending(userPhoneNumberIndexField);
         await userCollection.Indexes.CreateOneAsync(new CreateIndexModel<Models.User>(userPhoneNumberIndex, new CreateIndexOptions<Models.User>()
@@ -51,6 +69,15 @@ public class MongoContext
             )
         }));
 
+        // UpdatedAt
+        IndexKeysDefinition<Models.User> userUpdatedAtIndex = Builders<Models.User>.IndexKeys.Ascending(Models.User.UPDATED_AT);
+        await userCollection.Indexes.CreateOneAsync(new CreateIndexModel<Models.User>(userUpdatedAtIndex));
+
+        // CreateAt
+        IndexKeysDefinition<Models.User> userCreateAtIndex = Builders<Models.User>.IndexKeys.Ascending(Models.User.CREATED_AT);
+        await userCollection.Indexes.CreateOneAsync(new CreateIndexModel<Models.User>(userCreateAtIndex));
+
+        // FirstName.MiddleName.LastName
         IndexKeysDefinition<Models.User> userFullNameIndex = Builders<Models.User>.IndexKeys.Ascending(Models.User.FIRST_NAME).Ascending(Models.User.MIDDLE_NAME).Ascending(Models.User.LAST_NAME);
         await userCollection.Indexes.CreateOneAsync(new CreateIndexModel<Models.User>(userFullNameIndex, new CreateIndexOptions<Models.User>()
         {
@@ -62,6 +89,7 @@ public class MongoContext
             )
         }));
 
+        // Clients.RefreshToken.Code
         string refreshTokenCodeIndexField = Models.User.CLIENTS + "." + UserClient.REFRESH_TOKEN + "." + RefreshToken.CODE;
         IndexKeysDefinition<Models.User> refreshTokenCodeIndex = Builders<Models.User>.IndexKeys.Ascending(refreshTokenCodeIndexField);
         await userCollection.Indexes.CreateOneAsync(new CreateIndexModel<Models.User>(refreshTokenCodeIndex, new CreateIndexOptions<Models.User>()
@@ -72,6 +100,7 @@ public class MongoContext
             )
         }));
 
+        // Clients.RefreshToken.Value
         string refreshTokenValueIndexField = Models.User.CLIENTS + "." + UserClient.REFRESH_TOKEN + "." + RefreshToken.VALUE;
         IndexKeysDefinition<Models.User> refreshTokenValueIndex = Builders<Models.User>.IndexKeys.Ascending(Models.User.CLIENTS + "." + UserClient.REFRESH_TOKEN + "." + RefreshToken.VALUE);
         await userCollection.Indexes.CreateOneAsync(new CreateIndexModel<Models.User>(refreshTokenValueIndex, new CreateIndexOptions<Models.User>()
@@ -82,6 +111,7 @@ public class MongoContext
             )
         }));
 
+        // Clients.Token.Value
         string tokenValueIndexField = Models.User.CLIENTS + "." + UserClient.TOKEN + "." + Token.VALUE;
         IndexKeysDefinition<Models.User> tokenValueIndex = Builders<Models.User>.IndexKeys.Ascending(tokenValueIndexField);
         await userCollection.Indexes.CreateOneAsync(new CreateIndexModel<Models.User>(tokenValueIndex, new CreateIndexOptions<Models.User>()
