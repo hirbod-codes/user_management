eec61e95a79e6116f3fda17572e8cb35bb1e18a6
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-09-02 16:51:54 +0100

    Create IClientManagement.cs

---
+++ Controllers/Services/IClientManagement.cs
@@ -0,0 +1,26 @@
+using user_management.Models;
+
+namespace user_management.Controllers.Services;
+
+public interface IClientManagement
+{
+    /// <exception cref="user_management.Services.Client.RegistrationFailure"></exception>
+    /// <exception cref="user_management.Services.Data.DuplicationException"></exception>
+    /// <exception cref="user_management.Services.Data.DatabaseServerException"></exception>
+    public Task<(Models.Client client, string? notHashedSecret)> Register(Models.Client client);
+
+    /// <exception cref="System.ArgumentException"></exception>
+    public Task<Client?> RetrieveClientPublicInfo(string id);
+
+    /// <exception cref="System.ArgumentException"></exception>
+    public Task<Client?> RetrieveBySecret(string secret);
+
+    /// <exception cref="System.ArgumentException"></exception>
+    /// <exception cref="user_management.Services.Data.DuplicationException"></exception>
+    /// <exception cref="user_management.Services.Data.DatabaseServerException"></exception>
+    public Task UpdateRedirectUrl(string clientId, string clientSecret, string redirectUrl);
+
+    /// <exception cref="System.ArgumentException"></exception>
+    /// <exception cref="user_management.Services.Data.DataNotFoundException"></exception>
+    public Task DeleteBySecret(string clientId, string secret);
+}
