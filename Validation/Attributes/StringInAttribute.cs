22d11f40f6f6d63475c18dadef73b85972005e9e
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-09-02 16:55:21 +0100

    Create StringInAttribute.cs

---
+++ Validation/Attributes/StringInAttribute.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace user_management.Validation.Attributes;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+public class StringInAttribute : ValidationAttribute
+{
+    public string[] Strings { get; private set; }
+
+    public StringInAttribute(string[] strings) => Strings = strings;
+
+    public override bool IsValid(object? value) => value != null && ((string)value) == null && Strings.Contains((string)value!);
+}
