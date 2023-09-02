23c36d5e1fe11f766536aebbd8830c3728f963be
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-09-02 16:21:23 +0100

    Create IAuthenticatedByBearer.cs

---
+++ Authentication/IAuthenticatedByBearer.cs
@@ -0,0 +1,15 @@
+using user_management.Models;
+
+namespace user_management.Authentication;
+
+public interface IAuthenticatedByBearer
+{
+    /// <returns>True id there is an authenticated entity, false otherwise.</returns>
+    public bool IsAuthenticated();
+
+    /// <returns>The Authenticated Client</returns>
+    /// <exception cref="System.Security.Authentication.AuthenticationException"></exception>
+    public Task<UserClient> GetAuthenticated();
+
+    public void SetAuthenticated(UserClient authenticatedUserClient);
+}
