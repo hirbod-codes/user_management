22434e3acdff3ed817edf7bb3e19a3cafc48a83a
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-09-02 16:55:30 +0100

    Update TokenAuthDto.cs

--- Dtos/Token/TokenAuthDto.cs
+++ Dtos/Token/TokenAuthDto.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using user_management.Validation.Attributes;
 
 namespace user_management.Dtos.Token;
@@ -7,8 +8,10 @@ public class TokenAuthDto
     [ObjectId]
     public string ClientId { get; set; } = null!;
     public string RedirectUrl { get; set; } = null!;
+    [MinLength(40)]
     public string State { get; set; } = null!;
     public string CodeChallenge { get; set; } = null!;
+    [StringIn(new string[] { "SHA256", "SHA512" })]
     public string CodeChallengeMethod { get; set; } = null!;
     public TokenPrivilegesCreateDto Scope { get; set; } = null!;
     public string ResponseType { get; set; } = null!;
