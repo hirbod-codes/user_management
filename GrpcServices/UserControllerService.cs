f5684a1f1d4b629e319454b7e5bed8baf5a1a035
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-09-03 21:21:31 +0100

    Update UserControllerService.cs

--- GrpcServices/UserControllerService.cs
+++ GrpcServices/UserControllerService.cs
@@ -1,35 +1,23 @@
 namespace user_management.GrpcServices;
 
-using AutoMapper;
 using Grpc.Core;
 using MongoDB.Bson;
 using Newtonsoft.Json;
 using user_management.Models;
 using user_management.Services;
-using user_management.Services.Data.User;
 using user_management.Utilities;
 
 public class UserControllerService : UserController.UserControllerBase
 {
-    private const int EXPIRATION_MINUTES = 6;
     private readonly UserManagement _userManagement;
-    private readonly IMapper _mapper;
-    private readonly IUserRepository _userRepository;
-    private readonly IAuthHelper _authHelper;
     private readonly IStringHelper _stringHelper;
-    private readonly INotificationHelper _notificationHelper;
-    private readonly IDateTimeProvider _dateTimeProvider;
 
-    public UserControllerService(IUserRepository userRepository, IMapper mapper, IStringHelper stringHelper, INotificationHelper notificationHelper, IAuthHelper authHelper, IDateTimeProvider dateTimeProvider, UserManagement userManagement)
+    public UserControllerService(IStringHelper stringHelper, UserManagement userManagement)
     {
         _userManagement = userManagement;
-        _mapper = mapper;
-        _userRepository = userRepository;
-        _authHelper = authHelper;
-        _notificationHelper = notificationHelper;
         _stringHelper = stringHelper;
-        _dateTimeProvider = dateTimeProvider;
     }
+
     public override async Task<GetResponse> Get(GetRequest request, ServerCallContext context)
     {
         PartialUser user = await _userManagement.RetrieveById(request.AuthorId, request.Id, request.IsClient);
@@ -42,7 +30,7 @@ public class UserControllerService : UserController.UserControllerBase
             User = new Google.Protobuf.WellKnownTypes.Any()
             {
                 TypeUrl = "#",
-                Value = Google.Protobuf.ByteString.CopyFrom(Convert.FromBase64String((new StringHelper()).Base64Encode(JsonConvert.SerializeObject(content))))
+                Value = Google.Protobuf.ByteString.CopyFrom(Convert.FromBase64String(_stringHelper.Base64Encode(JsonConvert.SerializeObject(content))))
             }
         };
     }
