6884e89d500c9b7d7055c1a7f2df88464b2254a8
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-09-08 15:13:01 +0100

    Update Seeder.cs

--- Data/Seeder.cs
+++ Data/Seeder.cs
@@ -1,30 +1,25 @@
 namespace user_management.Data;
 
-using Microsoft.Extensions.Options;
 using user_management.Data.Seeders;
-using user_management.Services.Data.User;
 
 public class Seeder
 {
-    private readonly IUserRepository _userRepository;
     private readonly string _rootPath;
-
     public MongoContext MongoContext { get; set; }
 
-    public Seeder(IOptions<MongoContext> mongoContext, IUserRepository userRepository, string rootPath)
+    public Seeder(MongoContext mongoContext, string rootPath)
     {
-        MongoContext = mongoContext.Value;
-        _userRepository = userRepository;
+        MongoContext = mongoContext;
         _rootPath = rootPath;
     }
 
     public async Task Seed()
     {
-        System.Console.WriteLine("Seeding...");
+        System.Console.WriteLine("\nSeeding...");
 
-        await (new ClientSeeder(MongoContext, _rootPath)).Seed();
-        await (new UserSeeder(MongoContext, _rootPath)).Seed();
+        await ClientSeeder.Seed(MongoContext, _rootPath, count: 10);
+        await UserSeeder.Seed(MongoContext, _rootPath, count: 100);
 
-        System.Console.WriteLine("Seeded...");
+        System.Console.WriteLine("Seeded...\n");
     }
 }
