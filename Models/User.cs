0fcc5830a51535e520530534a7f8fd1e6f08023c
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-09-08 14:47:22 +0100

    Add FakeUser method to User model

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,10 @@
 namespace user_management.Models;
 
 using System;
-using System.Collections;
 using System.Dynamic;
 using System.Reflection;
 using AutoMapper;
+using Bogus;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using user_management.Data;
@@ -267,6 +267,160 @@ public class User : IEquatable<User>
         }
 
         return true;
+    }
+
+    public static User FakeUser(IEnumerable<User>? users = null, IEnumerable<Client>? clients = null, FakeUserOptions? fakeUserOptions = null)
+    {
+        if (users == null) users = new User[] { };
+        if (clients == null) clients = new Client[] { };
+        if (fakeUserOptions == null) fakeUserOptions = new();
+
+        List<Privilege> privileges = StaticData.Privileges;
+
+        Faker faker = new();
+
+        User user = new()
+        {
+            Id = ObjectId.GenerateNewId(),
+            FirstName = faker.Random.Bool(0.6f) ? faker.Name.FirstName() : null,
+            MiddleName = faker.Random.Bool(0.6f) ? faker.Name.FirstName() : null,
+            LastName = faker.Random.Bool(0.6f) ? faker.Name.LastName() : null,
+            Email = faker.Internet.ExampleEmail(),
+            Username = faker.Internet.UserName(),
+            Password = faker.Internet.Password(),
+            PhoneNumber = faker.Random.Bool(0.4f) ? faker.Phone.PhoneNumber() : null,
+            IsVerified = faker.Random.Bool(0.8f),
+            LoggedOutAt = faker.Date.Between(DateTime.UtcNow.AddDays(-7), DateTime.UtcNow.AddDays(+14)),
+            CreatedAt = faker.Date.Between(DateTime.UtcNow.AddDays(-7), DateTime.UtcNow.AddDays(+14))
+        };
+
+        do
+        {
+            if (users.FirstOrDefault<User?>(u => u != null && u.Username == user.Username) != null)
+                user.Username = faker.Random.String2(50);
+            else if (users.FirstOrDefault<User?>(u => u != null && user.FirstName != null && u.FirstName == user.FirstName) != null)
+                user.FirstName = faker.Random.Bool(0.6f) ? faker.Random.String2(50) : null;
+            else if (users.FirstOrDefault<User?>(u => u != null && user.MiddleName != null && u.MiddleName == user.MiddleName) != null)
+                user.MiddleName = faker.Random.Bool(0.6f) ? faker.Random.String2(50) : null;
+            else if (users.FirstOrDefault<User?>(u => u != null && user.LastName != null && u.LastName == user.LastName) != null)
+                user.LastName = faker.Random.Bool(0.6f) ? faker.Random.String2(50) : null;
+            else if (users.FirstOrDefault<User?>(u => u != null && user.PhoneNumber != null && u.PhoneNumber == user.PhoneNumber) != null)
+                user.PhoneNumber = faker.Random.Bool(0.6f) ? faker.Random.String2(50) : null;
+            else break;
+        } while (true);
+
+        user.UpdatedAt = user.CreatedAt.AddHours(faker.Random.Int(2, 96));
+
+        IEnumerable<Privilege> userPrivileges = null!;
+        if (fakeUserOptions.RandomPrivileges) user.Privileges = faker.PickRandom<Privilege>(privileges, faker.Random.Int(0, privileges.Count())).ToArray();
+        else user.Privileges = privileges.ToArray();
+
+        if (fakeUserOptions.RandomClients && clients.Count() > 1)
+        {
+            IEnumerable<Client> pickedClients = faker.PickRandom<Client>(clients, faker.Random.Int(0, clients.Count()));
+            for (int i = 0; i < pickedClients.Count(); i++)
+                user.Clients = user.Clients.Append(UserClient.FakeUserClient(pickedClients.ElementAt(i), userPrivileges)).ToArray();
+        }
+        else if (fakeUserOptions.RandomClients && clients.Count() == 1 && faker.Random.Bool())
+            user.Clients = user.Clients.Append(UserClient.FakeUserClient(clients.ElementAt(0), userPrivileges)).ToArray();
+        else if (!fakeUserOptions.RandomClients && clients.Count() > 1)
+            for (int i = 0; i < clients.Count(); i++)
+                user.Clients = user.Clients.Append(UserClient.FakeUserClient(clients.ElementAt(i), userPrivileges)).ToArray();
+        else if (!fakeUserOptions.RandomClients && clients.Count() == 1 && faker.Random.Bool())
+            user.Clients = user.Clients.Append(UserClient.FakeUserClient(clients.ElementAt(0), userPrivileges)).ToArray();
+
+        if (fakeUserOptions.GiveUserPrivilegesToRandomUsers)
+        {
+            IEnumerable<User> pickedUsers = faker.PickRandom<User>(users, faker.Random.Int(0, users.Count()));
+            for (int i = 0; i < pickedUsers.Count(); i++)
+            {
+                if (faker.Random.Bool())
+                    user.UserPrivileges.Readers = user.UserPrivileges.Readers.Append(new()
+                    {
+                        Author = Reader.USER,
+                        AuthorId = pickedUsers.ElementAt(i).Id,
+                        IsPermitted = faker.Random.Bool(0.8f),
+                        Fields = faker.PickRandom(User.GetReadableFields(), faker.Random.Int(0, User.GetReadableFields().Count())).ToArray()
+                    }).ToArray();
+
+                if (faker.Random.Bool())
+                {
+                    Field[] acceptableFields = User.GetUpdatableFields().Where(f =>
+                        user.UserPrivileges.Readers.FirstOrDefault(r =>
+                            r.AuthorId == pickedUsers.ElementAt(i).Id
+                            && r.IsPermitted
+                            && r.Fields.FirstOrDefault(readerField => readerField.Name == f.Name && readerField.IsPermitted) != null
+                        ) != null).ToArray();
+
+                    user.UserPrivileges.Updaters = user.UserPrivileges.Updaters.Append(new()
+                    {
+                        Author = Updater.USER,
+                        AuthorId = pickedUsers.ElementAt(i).Id,
+                        IsPermitted = faker.Random.Bool(0.8f),
+                        Fields = faker.PickRandom(acceptableFields, faker.Random.Int(0, acceptableFields.Count())).ToArray()
+                    }).ToArray();
+                }
+
+                if (faker.Random.Bool()) user.UserPrivileges.Deleters = user.UserPrivileges.Deleters.Append(new()
+                {
+                    Author = Deleter.USER,
+                    AuthorId = pickedUsers.ElementAt(i).Id,
+                    IsPermitted = faker.Random.Bool()
+                }).ToArray();
+
+                if (faker.Random.Bool()) user.UserPrivileges.AllReaders = new() { ArePermitted = faker.Random.Bool(0.8f), Fields = faker.PickRandom<Field>(User.GetReadableFields(), faker.Random.Int(0, User.GetReadableFields().Count())).ToArray() };
+
+                if (faker.Random.Bool())
+                {
+                    Field[] acceptableFields = User.GetUpdatableFields().Where(f =>
+                        user.UserPrivileges.AllReaders != null
+                        && user.UserPrivileges.AllReaders.ArePermitted
+                        && user.UserPrivileges.AllReaders.Fields.FirstOrDefault(allReadersField => allReadersField.Name == f.Name && allReadersField.IsPermitted) != null).ToArray();
+
+                    user.UserPrivileges.AllUpdaters = new() { ArePermitted = faker.Random.Bool(0.8f), Fields = faker.PickRandom<Field>(acceptableFields, faker.Random.Int(0, acceptableFields.Count())).ToArray() };
+                }
+            }
+        }
+
+        if (fakeUserOptions.GiveUserPrivilegesToItSelf) user.UserPrivileges = new()
+        {
+            Readers = new Reader[] { new() { Author = Reader.USER, AuthorId = user.Id, IsPermitted = true, Fields = User.GetReadableFields().ToArray() } },
+            AllReaders = new() { ArePermitted = false, Fields = new Field[] { } },
+            Updaters = new Updater[] { new() { Author = Updater.USER, AuthorId = user.Id, IsPermitted = true, Fields = User.GetUpdatableFields().ToArray() } },
+            AllUpdaters = new() { ArePermitted = false, Fields = new Field[] { } },
+        };
+
+        // Giving privileges to authorized clients.
+        for (int j = 0; j < user.Clients.Count(); j++)
+        {
+            if (user.Clients[j].RefreshToken == null) continue;
+
+            if (user.Clients[j].RefreshToken!.TokenPrivileges.ReadsFields.Length > 0)
+                user.UserPrivileges.Readers = user.UserPrivileges.Readers.Append(new()
+                {
+                    Author = Reader.CLIENT,
+                    AuthorId = user.Clients[j].ClientId,
+                    IsPermitted = faker.Random.Bool(0.8f),
+                    Fields = user.Clients[j].RefreshToken!.TokenPrivileges.ReadsFields
+                }).ToArray();
+            if (user.Clients[j].RefreshToken!.TokenPrivileges.UpdatesFields.Length > 0)
+                user.UserPrivileges.Updaters = user.UserPrivileges.Updaters.Append(new()
+                {
+                    Author = Updater.CLIENT,
+                    AuthorId = user.Clients[j].ClientId,
+                    IsPermitted = faker.Random.Bool(0.8f),
+                    Fields = user.Clients[j].RefreshToken!.TokenPrivileges.UpdatesFields
+                }).ToArray();
+            if (user.Clients[j].RefreshToken!.TokenPrivileges.DeletesUser)
+                user.UserPrivileges.Deleters = user.UserPrivileges.Deleters.Append(new()
+                {
+                    Author = Deleter.CLIENT,
+                    AuthorId = user.Clients[j].ClientId,
+                    IsPermitted = faker.Random.Bool(0.8f)
+                }).ToArray();
+        }
+
+        return user;
     }
 
     public override bool Equals(object? obj) => obj != null && Equals(obj as User);
