85ef650de70d3c0152f6309e366642a386acaa08
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-09-21 11:14:22 +0100

    Fix PrivilegesValidationAttribute validation attribute class

--- src/user_management/Validation/Attributes/PrivilegesValidationAttribute.cs
+++ src/user_management/Validation/Attributes/PrivilegesValidationAttribute.cs
@@ -7,5 +7,5 @@ namespace user_management.Validation.Attributes;
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
 public class PrivilegesValidationAttribute : ValidationAttribute
 {
-    public override bool IsValid(object? value) => value != null && ((IEnumerable<Privilege>)value) == null && StaticData.AreValid((IEnumerable<Privilege>)value);
+    public override bool IsValid(object? value) => value != null && ((IEnumerable<Privilege>)value) != null && StaticData.AreValid((IEnumerable<Privilege>)value);
 }
