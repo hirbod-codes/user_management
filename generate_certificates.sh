7ddb873d49105b948d3eb65eeb4541acb91b8741
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-10-11 18:43:11 +0100

    Update generate_certificates.sh

--- generate_certificates.sh
+++ generate_certificates.sh
@@ -40,9 +40,40 @@ openssl genrsa -out $projectRootDirectory/security/ca/ca.key
 openssl req -x509 -sha256 -config ca.cnf -key $projectRootDirectory/security/ca/ca.key -out $projectRootDirectory/security/ca/ca.crt -subj /O=user_management/OU=mongodb/CN=user_management_certificate_authority
 echo ""
 
+# Clients
 bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/localhost --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb_client --cn localhost"
 bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb_client --cn user_management --san user_management -shouldExportPkcs12"
 
+# Servers
+
+## Sharded cluster
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_mongodb --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn user_management_mongodb --san user_management_mongodb"
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_mongodb_member --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn member --san user_management_mongodb"
+
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_configServer1 --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn user_management_configServer1 --san user_management_configServer1"
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_configServer1_member --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn member --san user_management_configServer1"
+
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_configServer2 --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn user_management_configServer2 --san user_management_configServer2"
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_configServer2_member --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn member --san user_management_configServer2"
+
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_configServer3 --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn user_management_configServer3 --san user_management_configServer1"
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_configServer3_member --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn member --san user_management_configServer3"
+
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_shardServer1 --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn user_management_shardServer1 --san user_management_shardServer1"
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_shardServer1_member --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn member --san user_management_shardServer1"
+
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_shardServer2 --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn user_management_shardServer2 --san user_management_shardServer2"
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_shardServer2_member --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn member --san user_management_shardServer2"
+
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_shardServer3 --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn user_management_shardServer3 --san user_management_shardServer3"
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_shardServer3_member --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn member --san user_management_shardServer3"
+
+## Replica Set
 bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_replicaSet_p --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn user_management_replicaSet_p --san user_management_replicaSet_p"
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_replicaSet_p_member --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn member --san user_management_replicaSet_p"
+
 bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_replicaSet_s_1 --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn user_management_replicaSet_s_1 --san user_management_replicaSet_s_1"
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_replicaSet_s_1_member --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn member --san user_management_replicaSet_s_1"
+
 bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_replicaSet_s_2 --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn user_management_replicaSet_s_2 --san user_management_replicaSet_s_2"
+bash -c "./generate_certificate.sh --dir $projectRootDirectory/security/user_management_replicaSet_s_2_member --caCrt $projectRootDirectory/security/ca/ca.crt --caKey $projectRootDirectory/security/ca/ca.key --configFile client.cnf --extensions req_ext --ou mongodb --cn member --san user_management_replicaSet_s_2"
