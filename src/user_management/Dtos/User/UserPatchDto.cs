4decfb9f6eabb14b42b6b36e61b0defc3d89b2fc
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-10-18 18:51:13 +0100

    Add XML comments for UpdateLogics.BuildILogic and FilterLogics.BuildILogic methods

--- src/user_management/Dtos/User/UserPatchDto.cs
+++ src/user_management/Dtos/User/UserPatchDto.cs
@@ -4,8 +4,15 @@ namespace user_management.Dtos.User;
 
 public class UserPatchDto
 {
+    /// <summary>
+    /// <include file='./docs/xml.xml' path='user_management/Data/Logics/Filter/FilterLogicsGeneric/BuildILogic/logicsString' />
+    /// </summary>
     [FiltersString]
     public string? FiltersString { get; set; }
+
+    /// <summary>
+    /// <include file='./docs/xml.xml' path='user_management/Data/Logics/Update/UpdateLogicsGeneric/BuildILogic/updatesString' />
+    /// </summary>
     [UpdatesString]
     public string? UpdatesString { get; set; }
 }
