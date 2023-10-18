5d1af295d280642181124311bbc6b38f2cc95caf
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-10-19 00:03:15 +0100

    Forgot to remove comments

--- src/user_management/Utilities/NotificationHelper.cs
+++ src/user_management/Utilities/NotificationHelper.cs
@@ -13,21 +13,3 @@ here's your verification code: {verificationCode}
 
 Regards <b>user_management</b>", "Verification email from user management", new() { IsBodyHtml = true });
 }
-
-// MailMessage mail = new()
-// {
-//     From = new MailAddress("taghalloby@gmail.com"),
-//     Subject = "Verification email from user_management",
-//     IsBodyHtml = true,
-//     Body = $@"",
-// };
-// mail.To.Add(email);
-
-// SmtpClient smtpClient = new()
-// {
-//     Host = "smtp.gmail.com",
-//     Port = 587,
-//     Credentials = new NetworkCredential("taghalloby@gmail.com", "ihtz hgea bxnt gqya"),
-//     EnableSsl = true
-// };
-// await smtpClient.SendMailAsync(mail);
